3cec6e241b31a2a9336b7f545ea60d5d36e6af72
AmanyHmidan <amanyhmidan@gmail.com>
2024-07-29 14:31:54 +0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const hiddenHeader = document.querySelector(".hidden-header");
+
+  menuToggle.addEventListener("click", function () {
+    let nav = document.querySelector(".hidden-header");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  });
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 750) {
+      hiddenHeader.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const facebook = document.getElementById("Facebook"); // Use document directly
+  facebook.addEventListener("click", function () {
+    const url = window.location.href;
+    window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url),
+        "facebook-share-dialog",
+        "width=800,height=600");
+    return false;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const LinkedIn = document.getElementById("LinkedIn"); // Use document directly
+  LinkedIn.addEventListener("click", function () {
+    const url = "https://www.yourwebsite.com";
+    window.open("https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURIComponent(url),
+        "linkedin-share-dialog",
+        "width=800,height=600");
+    return false;
+  });
+});
+
+
+
